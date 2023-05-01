45b65cb511aaff19dfa9b29d235a44b84cd6a95b
wxw183 <1404687410@qq.com>
2023-05-01 23:49:29 +0800

    202305012349

---
+++ generator.bat
@@ -0,0 +1,35 @@
+mkdir 01
+copy /y adi.exe 01
+mkdir 02
+copy /y adi.exe 02
+mkdir 03
+copy /y adi.exe 03
+mkdir 04
+copy /y adi.exe 04
+mkdir 05
+copy /y adi.exe 05
+mkdir 06
+copy /y adi.exe 06
+mkdir 07
+copy /y adi.exe 07
+cd 01
+adi 0.1 0.02 10
+cd ..
+cd 02
+adi 0.05 0.02 10
+cd ..
+cd 03
+adi 0.025 0.02 10
+cd ..
+cd 04
+adi 0.05 0.005 10
+cd ..
+cd 05
+adi 0.05 0.01 10
+cd ..
+cd 06
+adi 0.05 0.02 10
+cd ..
+cd 07
+adi 0.05 0.04 10
+cd ..
